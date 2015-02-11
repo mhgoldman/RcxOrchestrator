cfb8e8023ee601a4d21113a7f9d2204d1d8270d2
Martin Goldman <martin@mgoldman.com>
2015-02-10 21:27:57 -0500

    oops, uncomment other tests

--- spec/models/rcx_skytap/skytap_client_spec.rb
+++ spec/models/rcx_skytap/skytap_client_spec.rb
@@ -37,23 +37,21 @@ RSpec.describe RcxSkytap::SkytapClient, :type => :model do
 		end
 	end
 
-	
+	context ".fetch_for_user(user)" do
+		it "gets skytap VMs for user" do
+			VCR.use_cassette("fetched_skytap_vms") do
+				clients_for_user = RcxSkytap::SkytapClient.fetch_for_user(u)
+				expect(clients_for_user.count).to eq 2
+			end
 
-	# context ".fetch_for_user(user)" do
-	# 	it "gets skytap VMs for user" do
-	# 		VCR.use_cassette("fetched_skytap_vms") do
-	# 			clients_for_user = RcxSkytap::SkytapClient.fetch_for_user(u)
-	# 			expect(clients_for_user.count).to eq 2
-	# 		end
-
-	# 		VCR.use_cassette("fetched_skytap_vm_details") do
-	# 			clients_for_user = RcxSkytap::SkytapClient.fetch_for_user(u)
-	# 			skytap_vm = clients_for_user.first.get_skytap_vm
-	# 			expect(skytap_vm.runstate).to eq 'stopped'
-	# 			expect(skytap_vm.id).to eq '4524114'
-	# 			expect(skytap_vm.name).to eq 'Sharepoint Server 2013 Trial - Windows Server 2012 Standard - Eval'
-	# 			expect(skytap_vm.configuration_url).to eq 'https://cloud.skytap.com/configurations/3287576'
-	# 		end
-	# 	end
-	# end
+			VCR.use_cassette("fetched_skytap_vm_details") do
+				clients_for_user = RcxSkytap::SkytapClient.fetch_for_user(u)
+				skytap_vm = clients_for_user.first.get_skytap_vm
+				expect(skytap_vm.runstate).to eq 'stopped'
+				expect(skytap_vm.id).to eq '4524114'
+				expect(skytap_vm.name).to eq 'Sharepoint Server 2013 Trial - Windows Server 2012 Standard - Eval'
+				expect(skytap_vm.configuration_url).to eq 'https://cloud.skytap.com/configurations/3287576'
+			end
+		end
+	end
 end
